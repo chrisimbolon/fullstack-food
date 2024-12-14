98116ffaf7f8917578d8e0aeb10f0f42147fe9ec
Christyan Simbolon <sambungdata@gmail.com>
2024-12-14 23:44:10 +0700

    starting on toggle responsive nav

--- js/script.js
+++ js/script.js
@@ -1,10 +1,17 @@
-// Fixing flexbox gap property missing in some Safari versions
-
-console.log("lauk pauk");
-
 const yearEl = document.querySelector(".year");
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Mobile Nav
+
+const btnNav = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNav.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Fixing flexbox gap property missing in some Safari versions
 
 function checkFlexGap() {
   var flex = document.createElement("div");
